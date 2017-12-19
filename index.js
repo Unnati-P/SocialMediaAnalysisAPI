bcb3e8f8e4b98b57fa6ad88b75fe22d00a364820
Unnati Parekh <unnati.parekh502@gmail.com>
2017-12-19 23:32:59 +0530

    fetch tweets for network

--- index.js
+++ index.js
@@ -19,12 +19,202 @@ app.use(function (req, res, next) {
 MongoClient.connect('mongodb://ajay:123456@ds135186.mlab.com:35186/tweets', function(err, db) {
   console.log("Connected successfully to server");
 
-  app.get("/fetchtweets", function (req, res) {
+ app.get("/fetchtweets", function (req, res) {
     console.log("fetching tweets");
     db.collection("ockhitweets").find({}, {retweeted: 1}).limit(100).toArray(function(err, docs){
       return res.send({results: docs});
     })
   });
+
+  app.get("/getmentionsnodes", function (req, res) {
+    var query = db.collection("ockhidump").find({}, {"user.screen_name": 1, "entities.user_mentions": 1});
+
+    query.toArray(function(err, docs){
+      var count=1;
+      const nodes = [];
+     _.forEach(docs, function(value1, key1){
+          var data = {
+            id : value1.user.screen_name,
+            group : count
+          };
+          nodes.push(data);
+
+          _.forEach(value1.entities, function(value2, key2) {
+            var c =0;
+            if(!_.isEmpty(value2)) {
+
+            var data = {
+              id : value2[c].screen_name,
+              //group : count
+            };
+            c++;
+            //console.log(value2);
+            nodes.push(data);
+            }
+          })
+        count++;
+        })
+
+
+
+      //console.log(count);
+      return res.send(nodes);
+
+  })
+ });
+
+ app.get("/getmentionslinks", function (req, res) {
+   var query = db.collection("ockhidump").find({}, {"user.screen_name": 1, "entities.user_mentions": 1});
+
+   query.toArray(function(err, docs){
+
+     var links = [];
+     var count=1;
+     var nodes = [];
+    _.forEach(docs, function(value1, key1){
+         var data = {
+           id : value1.user.screen_name,
+           //group : count
+         };
+         nodes.push(data);
+
+         _.forEach(value1.entities, function(value2, key2) {
+           var c =0;
+           if(!_.isEmpty(value2)) {
+
+           var data = {
+             id : value2[c].screen_name,
+             //group : count
+           };
+           c++;
+           //console.log(value2);
+           nodes.push(data);
+           }
+         })
+       count++;
+       })
+       var newNodes = _.uniqWith(nodes, _.isEqual);
+
+    _.forEach(docs, function(value1, key1){
+         _.forEach(value1.entities, function(value2, key2) {
+           var c =0;
+           if(!_.isEmpty(value2)) {
+
+           var data = {
+             source : value1.user.screen_name,
+             target : value2[c].screen_name,
+             //value : 1
+           };
+           c++;
+           //console.log(value2);
+           links.push(data);
+           }
+         })
+       count++;
+       })
+       var newLinks = _.uniqWith(links, _.isEqual);
+
+       const parent = {
+         "nodes" : newNodes,
+         "links" : newLinks
+       };
+
+
+     console.log(count);
+     return res.send( parent);
+
+ })
+ });
+
+  app.get("/getgraph", function (req, res) {
+    var query = db.collection("pollutiondump").find({}, {"user.screen_name": 1, "entities.user_mentions": 1});
+
+    query.toArray(function(err, docs){
+
+      const parent = {};
+      _.forEach(docs, function(value1, key1){
+          _.forEach(value1.entities.user_mentions, function(value2, key2){
+            var arr = [];
+            parent[value2.screen_name] = arr;
+          })
+
+          _.forEach(value1.entities.user_mentions, function(value4, key4){
+            var name = value1.user.screen_name;
+              console.log(parent[value4.screen_name]);
+          //  console.log(value1.user.screen_name, "-->", value4.screen_name);
+            parent[value4.screen_name].push(name);
+
+          })
+      })
+      //console.log(count);
+      return res.send({"results": parent});
+
+  })
+});
+
+app.get("/getnodes", function (req, res) {
+  var query = db.collection("ockhidump").find({}, {"user.screen_name": 1});
+
+  query.toArray(function(err, docs){
+
+    const nodes = [];
+    _.forEach(docs, function(value, key) {
+      var data = {
+          label :  value.user.screen_name
+      }
+      nodes.push(data);
+      //console.log(value.user.screen_name);
+    })
+    //console.log(resu);
+      return res.send(nodes);
+  });
+
+
+});
+
+app.get("/getotherreply", function (req, res) {
+  var query = db.collection("ockhidump").find({}, {"user.screen_name": 1, "in_reply_to_screen_name": 1});
+
+  query.toArray(function(err, docs){
+
+    const nodes = [];
+    _.forEach(docs, function(value, key) {
+      var data = {
+          label :  value.user.screen_name
+      }
+      nodes.push({ node : data});
+      //console.log(value.user.screen_name);
+    })
+    //console.log(resu);
+      return res.send(nodes);
+  });
+
+
+});
+
+app.get("/getlinksreply", function (req, res) {
+  var query = db.collection("ockhidump").find({}, {"user.screen_name": 1, "in_reply_to_screen_name": 1 });
+
+  query.toArray(function(err, docs){
+
+    const links = [];
+    _.forEach(docs, function(value, key) {
+      if(!_.isEmpty(value.in_reply_to_screen_name)) {
+      var data = {
+          source :  value.user.screen_name,
+          target : value.in_reply_to_screen_name,
+          weight : 1
+      };
+      links.push(data);
+    }
+      //console.log(value.user.screen_name);
+    })
+    //console.log(resu);
+      return res.send(links);
+  });
+
+
+});
 
   //This endpoint will give the number of tweets retweeted in case of smog in Delhi
   app.get("/getpollutionretweets", function (req, res) {
@@ -127,7 +317,7 @@ MongoClient.connect('mongodb://ajay:123456@ds135186.mlab.com:35186/tweets', func
           resu[key] = value.length;
         }
       })
-      //key: 0-5, 5-10, values: numbers
+
       return res.send({"results": resu});
     })
   });
